4c7b1b953cd64539893439a6413bf4de5248d785
Michael Beierwaltes <mbeierwaltes321@gmail.com>
2026-06-06 14:23:00 -0500

    Cleaned up app footer and made it more vuetify-compliant

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 
-import { VContainer, VRow, VCol } from 'vuetify/components';
+import { VContainer, VRow, VCol, VImg } from 'vuetify/components';
+import gitHubIconPath from "@/assets/github-mark-white.svg";
+import linkedInIconPath from "@/assets/LI-In-Bug.png";
+import spotifyIconPath from "@/assets/Spotify_Icon_RGB_White.png";
 
-//Define interfaces for the footer data
 interface FooterLinkData {
     text: string,
     link: string,
@@ -18,62 +20,77 @@ interface FoooterIconData {
     link: string
 }
 
-const gitHubIconPath : string = "src/assets/github-mark-white.svg";
-const linkedInIconPath : string = "src/assets/LI-In-Bug.png"; 
-const spotifyIconPath : string = "src/assets/Spotify_Icon_RGB_White.png";
-
 const imgProperties: FoooterIconData[] = [
-    { id: "logo-github", path: gitHubIconPath, width: "50px", length: "50px", link: "https://github.com/mbeierwaltes321"},
-    { id: "logo-spotify", path: spotifyIconPath, width: "50px", length: "50px", link: "https://open.spotify.com/user/1256937600?si=2b4a04ed6d3e4ac7"},
-    { id: "logo-linkedin", path: linkedInIconPath, width: "50px", length: "58.7963px", link: "https://www.linkedin.com/in/michael-beierwaltes-703a93172"},
+    { 
+        id: "logo-github", 
+        path: gitHubIconPath, 
+        width: "50px", 
+        length: "50px", 
+        link: "https://github.com/mbeierwaltes321"
+    },
+    { 
+        id: "logo-spotify", 
+        path: spotifyIconPath, 
+        width: "50px", 
+        length: "50px", 
+        link: "https://open.spotify.com/user/1256937600?si=2b4a04ed6d3e4ac7"
+    },
+    { 
+        id: "logo-linkedin", 
+        path: linkedInIconPath, 
+        width: "50px", 
+        length: "58.7963px", 
+        link: "https://www.linkedin.com/in/michael-beierwaltes-703a93172"
+    },
 ]
 
 const footerLinks: FooterLinkData[] = [
     //TODO Fill these with links for eventual routing
-    {text: "About Me", link: "", first: true},
-    {text: "Contact Me", link: "mailto:mbeierwaltes321@.com"},
-    {text: "Terms & Conditions", link: ""},
-
-    //TODO - Either get rid of this or put actual copyright information when needed
-    {text: "Copyright Information", link: "", last: true},
+    {
+        text: "About Me",
+        link: "",
+        first: true
+    },
+    {
+        text: "Terms & Conditions",
+        link: ""
+    },
 ]
 
 </script>
 
 <template>
-    <v-footer :absolute="true" 
-              :app="true"
-              border
-              color="rgb(75, 0, 0)"
-    >    
-        <div class="container-fluid vh-25">
-            <div id="Links" class="d-flex align-items-center h-25 mt-2">
-                <div v-for="imgProp in imgProperties"> 
-                    <a :href="imgProp.link">
-                        <img v-if="imgProp.path != ''" 
-                            :id="imgProp.id" 
-                            class="m-2" 
-                            :src="imgProp.path" 
-                            :length="imgProp.length"
-                            :width="imgProp.width"/>
-                        <p v-else id="tempIcon" class="m-2">
-                            TEMP
-                        </p>
-                    </a>
-                </div>
-            </div>
-            <v-container class="m-0">
-                <v-row class="h-75" no-gutters>
-                    <v-col v-for="footerLink in footerLinks"
-                        :class="(footerLink.last === true? 'mt-5' : '')"
-                        :cols="12">
-                        <a :href="footerLink.link" class="text-dark-emphasis">
+    <v-footer app
+        border
+        absolute
+        color="rgb(75, 0, 0)"
+    >
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="auto"> 
+                    <div class="d-flex align-center">
+                        <a v-for="imgProp in imgProperties" :href="imgProp.link">
+                            <v-img v-if="imgProp.path != ''"
+                                :id="imgProp.id"
+                                class="mx-6"
+                                :src="imgProp.path"
+                                :height="imgProp.length"
+                                :width="imgProp.width"
+                            />
+                        </a>
+                    </div>
+                </v-col>
+            </v-row>
+            <v-row justify="center">
+                <v-col v-for="footerLink in footerLinks" cols="auto">
+                    <div class="d-flex justify-center">
+                        <a :href="footerLink.link" class="text-cyan-accent-3">
                             {{ footerLink.text }}
                         </a>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </div>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-footer>
 </template>
 
